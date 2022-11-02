83237c1375765b60e566afa4ea02c7619b037949
Brian Rangel <brianrang13@gmail.com>
2022-11-02 00:09:26 -0400

    Added delete endpoint for book controller and enabled cors

--- src/main/java/BookReviewApp/controller/ReviewController.java
+++ src/main/java/BookReviewApp/controller/ReviewController.java
@@ -3,12 +3,10 @@ package BookReviewApp.controller;
 import BookReviewApp.service.ReviewService;
 import BookReviewApp.model.Review;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/review")
 public class ReviewController {
     @Autowired
